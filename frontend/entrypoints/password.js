a87910739060b591dbca6d6971dfdb5000d33b4d
Miguel Montalvo <miguel.montalvo@barrelny.com>
2022-07-08 15:33:15 -0700

    Extract components from global.js

--- frontend/entrypoints/password.js
+++ frontend/entrypoints/password.js
@@ -1,1 +1,11 @@
 import '@/components/password-modal';
+import '@/components/quantity-input';
+import '@/components/menu-drawer';
+import '@/components/header-drawer';
+import '@/components/modal-dialog';
+import '@/components/modal-opener';
+import '@/components/deferred-media';
+import '@/components/slider-component';
+import '@/components/slideshow-component';
+import '@/components/variant-selects';
+import '@/components/variant-radios';
--- frontend/entrypoints/theme.js
+++ frontend/entrypoints/theme.js
@@ -1,2 +1,12 @@
 import '@/components/cart-notification';
 import '@/components/details-modal';
+import '@/components/quantity-input';
+import '@/components/menu-drawer';
+import '@/components/header-drawer';
+import '@/components/modal-dialog';
+import '@/components/modal-opener';
+import '@/components/deferred-media';
+import '@/components/slider-component';
+import '@/components/slideshow-component';
+import '@/components/variant-selects';
+import '@/components/variant-radios';
